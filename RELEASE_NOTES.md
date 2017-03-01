70fae69a3e6f4b89707212dc88fd8c9f88e81982
Pierre Irrmann <pierre.irrmann@gmail.com>
2017-03-01 18:37:37 +0100

    Bump version to 0.0.1-beta

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,5 +1,2 @@
-### 1.0 - Unreleased
-* Who knows?
-
-#### 0.0.1-beta - March 1st 2017
+#### 0.0.1-beta - Unreleased
 * Initial release
--- src/FSketch.Behaviours/AssemblyInfo.fs
+++ src/FSketch.Behaviours/AssemblyInfo.fs
@@ -5,8 +5,8 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSketch.Behaviours")>]
 [<assembly: AssemblyProductAttribute("FSketch")>]
 [<assembly: AssemblyDescriptionAttribute("Yet another drawing DSL in F#")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
 [<assembly: AssemblyConfigurationAttribute("Release")>]
 do ()
 
@@ -14,6 +14,6 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FSketch.Behaviours"
     let [<Literal>] AssemblyProduct = "FSketch"
     let [<Literal>] AssemblyDescription = "Yet another drawing DSL in F#"
-    let [<Literal>] AssemblyVersion = "1.0"
-    let [<Literal>] AssemblyFileVersion = "1.0"
+    let [<Literal>] AssemblyVersion = "0.0.1"
+    let [<Literal>] AssemblyFileVersion = "0.0.1"
     let [<Literal>] AssemblyConfiguration = "Release"
