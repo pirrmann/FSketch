f4233ac63b2650714ed915ded2c7bad49a24f418
Pierre Irrmann <pierre.irrmann@gmail.com>
2017-05-27 09:31:17 +0200

    Bump version to 0.0.4

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,5 +1,11 @@
-#### 0.0.3-beta - Unreleased
+#### 0.0.5-beta - Unreleased
 * Who knows?
+
+#### 0.0.4-beta - May 27th 2017
+* Improved text cases and alignments
+
+#### 0.0.3-beta - March 29th 2017
+* Several improvements and support for single-path fonts - correctly deployed
 
 #### 0.0.2-beta - March 29th 2017
 * Several improvements and support for single-path fonts
--- src/FSketch.Winforms/AssemblyInfo.fs
+++ src/FSketch.Winforms/AssemblyInfo.fs
@@ -5,8 +5,8 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSketch.Winforms")>]
 [<assembly: AssemblyProductAttribute("FSketch")>]
 [<assembly: AssemblyDescriptionAttribute("Yet another drawing DSL in F#")>]
-[<assembly: AssemblyVersionAttribute("0.0.3")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.3")>]
+[<assembly: AssemblyVersionAttribute("0.0.4")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.4")>]
 [<assembly: AssemblyConfigurationAttribute("Release")>]
 do ()
 
@@ -14,6 +14,6 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FSketch.Winforms"
     let [<Literal>] AssemblyProduct = "FSketch"
     let [<Literal>] AssemblyDescription = "Yet another drawing DSL in F#"
-    let [<Literal>] AssemblyVersion = "0.0.3"
-    let [<Literal>] AssemblyFileVersion = "0.0.3"
+    let [<Literal>] AssemblyVersion = "0.0.4"
+    let [<Literal>] AssemblyFileVersion = "0.0.4"
     let [<Literal>] AssemblyConfiguration = "Release"
